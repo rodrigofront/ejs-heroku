1f5d9a09e0a38c83dc6d5f22edbd1e7c097793c5
Rodrigo Nogueira <fale.rodrigo@gmail.com>
2017-03-11 19:59:35 -0300

    test app

---
+++ web.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    title: 'EJS Demo',
+    text: 'Um teste de EJS'
+  });
+});
+
+app.listen(process.env.PORT || 5000);
